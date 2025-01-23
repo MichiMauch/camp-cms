efd6b09a960849f1917163879edc3563340d2b61
MichiMauch <michi.mauch@gmail.com>
2025-01-23 22:24:33 +0100

    test tailwind

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 const nextConfig = {
+  output: 'standalone',
   env: {
     TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
     TURSO_DATABASE_AUTH_TOKEN: process.env.TURSO_DATABASE_AUTH_TOKEN,
