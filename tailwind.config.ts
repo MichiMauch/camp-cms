0ab47fda4d3b735e6697ebf11e6b6b7f7881b148
MichiMauch <michi.mauch@gmail.com>
2025-01-24 15:33:58 +0100

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ import animate from "tailwindcss-animate"
 export default {
   darkMode: ["class"],
   content: [
-"./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+"./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
